08ee2b253d72ee328c41e99486ee7eecd56077ff
PKuczynski <przemek.ku2000@gmail.com>
2025-05-09 17:15:34 +0200

    Dodaj pliki projektów.

---
+++ Hotel Client/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using HotelReservation.OnlineReceptionImplService;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotel_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Login();
+        }
+
+        public async void Login()
+        {
+            var client = new OnlineReceptionImplClient();
+            try
+            {
+                var result = await client.loginAsync("User1", "Test123");
+
+                var userId = result.@return;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
